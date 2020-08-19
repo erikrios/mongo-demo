8d92cda0839d938a415dc219913f82e006a499c8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-19 20:50:46 +0700

    Add function to update the course

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Android Development Course',
+        name: 'Angular Course',
         author: 'Erik Rio Setiawan',
-        tags: ['android', 'kotlin'],
+        tags: ['backend', 'rest-api'],
         isPublished: true
     });
 
@@ -28,10 +28,20 @@ async function createCourse() {
 
 async function getCourses() {
     return await Course
-        .find({ author: 'Erik Rio Setiawan', isPublished: true })
+        .find({ author: /.*Erik.*/i, isPublished: true })
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
+}
+
+async function updateCourse(id) {
+    const course = await Course.findById(id);
+    if (!course) return;
+
+    course.name = 'Express.js';
+    course.author = 'Erik';
+
+    return await course.save();
 }
 
 async function run() {
